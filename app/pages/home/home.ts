ae9e7bcb5596e921c76846f629ba37ffa747a4b1
Steve McDonald <smcdonald@Steves-MacBook-Pro.local>
2016-08-09 18:59:38 -0500

    added functionality on home

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -6,48 +6,57 @@ import {NavController} from 'ionic-angular';
 })
 export class HomePage {
 
-  _disabledButton: boolean = true;
-  _previousTime: string = '';
-  _currentTime: string = '';
-  _anotherTime: string = '';
+// PRIVATE class variables
+  private _disabledButton: boolean = true;
+  private _previousTime: string[] = [];
+  private _currentTime: string = '';
+  private _anotherTime: string = '';
 
+// CONSTRUCTOR for the page class
   constructor(public navCtrl: NavController) {
     this._currentTime = new Date().getFullYear().toString() + " AD";
     this._anotherTime = this.pickRandomYear();
   }
 
-  get currentTime():string{
+// PUBLIC Class read-only properties
+  public get currentTime():string{
     return this._currentTime;
   }
 
-  get anotherTime():string{
+  public get anotherTime():string{
     return this._anotherTime;
   }
 
-  get disabledButton():boolean{
+  public get disabledButton():boolean{
     return this._disabledButton;
   }
 
+// PUBLIC Class methods
+  public timeTravel(){
+    this._previousTime.push( this._currentTime);
+    this._currentTime = this._anotherTime;
+    this._anotherTime = this.pickRandomYear();
+    this.checkGoBackEnabled();
+  }
+
+  public goBack(){
+    this._anotherTime = this._currentTime;
+    this._currentTime = this._previousTime.pop();
+    this.checkGoBackEnabled();
+  }
+
+// PRIVATE Class methods
   private pickRandomYear(oldestYear:number = -4000, MostRecentYear:number = 4000){
     let randomYear: Number = Math.floor( Math.random() * (MostRecentYear - oldestYear + 1)) + oldestYear;
     return (randomYear > 0) ? (randomYear).toString() + " AD" : (-randomYear).toString() + " BC"; 
   }
 
-  timeTravel(){
-    this._previousTime = this._currentTime;
-    this._currentTime = this._anotherTime;
-    this.pickRandomYear();
-    if( this._previousTime != ''){
-      this._disabledButton = true;
+  private checkGoBackEnabled(){
+    if( this._previousTime.length > 0 ){
+      this._disabledButton = null;  // needs to be NULL and not FALSE
     }else{
-      this._disabledButton = false;
+      this._disabledButton = true;
     }
-  }
-
-  goBack(){
-    this.anotherTime = this.currentTime;
-    this.currentTime = this._previousTime;
-    this._previousTime = '';
   }
 
 }
